8ade83d727aa54efc6a3cf5aed7cd4e5ff4590cb
emanuelhardwell <emanuelnoe16@gmail.com>
2020-11-11 22:34:38 -0600

    Module edit created

--- src/routes/link.js
+++ src/routes/link.js
@@ -30,7 +30,26 @@ router.get("/", async (req, res) => {
 
 router.get("/delete/:id_link", async (req, res) => {
   const id = req.params.id_link;
-  pool.query("delete from links where id_link = ?", [id]);
+  await pool.query("delete from links where id_link = ?", [id]);
+  res.redirect("/link");
+});
+
+router.get("/edit/:id_link", async (req, res) => {
+  const id = req.params.id_link;
+  const links = await pool.query("select * from links where id_link = ?", [id]);
+  console.log(links[0]);
+  res.render("links/edit", { links: links[0] });
+});
+
+router.post("/edit/:id_link", async (req, res) => {
+  const id = req.params.id_link;
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+  pool.query("update links set ? where id_link = ?", [newLink, id]);
   res.redirect("/link");
 });
 
